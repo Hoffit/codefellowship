328a1df43d56eec33163e2b80ce908fbe0d4e8d5
Hoffit <dehull_1@yahoo.com>
2018-12-20 23:42:39 -0800

    Styling Done

--- src/main/java/com/heally/springmvctraining/codefellowship/ApplicationUserController.java
+++ src/main/java/com/heally/springmvctraining/codefellowship/ApplicationUserController.java
@@ -18,6 +18,9 @@ import java.security.Principal;
 import java.util.ArrayList;
 import java.util.Date;
 
+/**
+ * The Spring controller for ApplicationUser blog app.
+ */
 @Controller
 public class ApplicationUserController {
 
@@ -33,61 +36,153 @@ public class ApplicationUserController {
     @Autowired
     private PostRepository postRepository;
 
+    /**
+     * The password hashing library.
+     */
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    /**
+     * The home route.
+     * @param p The Swing security principal.
+     * @param model The Spring Model object.
+     * @return The route to index.html.
+     */
+    @GetMapping("/")
+    public String home(
+                Principal p,
+            Model model) {
+        ApplicationUser loggedInUser = null;
+        if (p != null) {
+            loggedInUser = (ApplicationUser) ((UsernamePasswordAuthenticationToken) p).getPrincipal();
+        }
+        model.addAttribute("loggedInUser", loggedInUser);
+        return "index";
+    }
+
+    /**
+     * The login route.
+     * @param p The Swing security principal.
+     * @param model The Spring Model object.
+     * @return The route to login.html.
+     */
     @GetMapping("/login")
-    public String login(Model model) {
+    public String login(
+            Principal p,
+            Model model) {
+        ApplicationUser loggedInUser = null;
+        if (p != null) {
+            loggedInUser = (ApplicationUser) ((UsernamePasswordAuthenticationToken) p).getPrincipal();
+        }
+        model.addAttribute("loggedInUser", loggedInUser);
         return "login";
     }
 
+    /**
+     * The register route.
+     * @param p The Swing security principal.
+     * @param model The Spring Model object.
+     * @return The route to register.html.
+     */
     @GetMapping("/register")
-    public String index(Model model) {
+    public String index(
+            Principal p,
+            Model model) {
+        ApplicationUser loggedInUser = null;
+        if (p != null) {
+            loggedInUser = (ApplicationUser) ((UsernamePasswordAuthenticationToken) p).getPrincipal();
+        }
+        model.addAttribute("loggedInUser", loggedInUser);
         return "register";
     }
 
+    /**
+     * The users route.
+     * @param p The Swing security principal.
+     * @param model The Spring Model object.
+     * @return The route to users.html.
+     */
     @GetMapping("/users")
-    public String getUsers(Model model) {
+    public String getUsers(
+            Principal p,
+            Model model) {
         model.addAttribute("users", userRepository.findAll());
+        ApplicationUser loggedInUser = (ApplicationUser) ((UsernamePasswordAuthenticationToken)p).getPrincipal();
+        model.addAttribute("loggedInUser", loggedInUser);
         return "users";
     }
 
+    /**
+     * The user details route.
+     * @param p The Swing security principal.
+     * @param userId The user id.
+     * @param model The Spring Model object.
+     * @return
+     */
     @GetMapping("/users/{userId}")
     public String getUser(
             Principal p,
             @PathVariable long userId,
             Model model) {
         model.addAttribute("user", userRepository.findById(userId).get());
-        model.addAttribute("principal", p);
-        return "userDetail";
-    }
-
-    @GetMapping("/myProfile")
-    public String getUserProfile(
-            Principal p,
-            Model model) {
+        //model.addAttribute("principal", p);
         ApplicationUser loggedInUser = (ApplicationUser) ((UsernamePasswordAuthenticationToken)p).getPrincipal();
-        model.addAttribute("user", loggedInUser);
-        model.addAttribute("principal", p);
+        model.addAttribute("loggedInUser", loggedInUser);
         return "userDetail";
     }
 
+    /**
+     * 
+     * @param model The Spring Model object.
+     * @param p The Swing security principal.
+     * @return
+     */
+    @GetMapping("/myProfile")
+    public String getMyProfile(
+            Model model,
+            Principal p) {
+        ApplicationUser loggedInUser = (ApplicationUser) ((UsernamePasswordAuthenticationToken)p).getPrincipal();
+        model.addAttribute("user", userRepository.findById(loggedInUser.getId()).get());
+        model.addAttribute("loggedInUser", loggedInUser);
+        return "userDetail";
+    }
 
+    /**
+     * 
+     * @param p The Swing security principal.
+     * @param model The Spring Model object.
+     * @param firstName
+     * @param lastName
+     * @param userName
+     * @param p The Swing security principal.assword
+     * @param bio
+     * @param dateOfBirth
+     * @return
+     */
     @PostMapping("/register")
     public RedirectView create(
+            Principal p,
+            Model model,
             @RequestParam String firstName,
             @RequestParam String lastName,
             @RequestParam String userName,
             @RequestParam String password,
             @RequestParam String bio,
             @RequestParam @DateTimeFormat(pattern="yyyy-MM-dd") Date dateOfBirth) {
-        ApplicationUser user = new ApplicationUser(firstName, lastName, userName, bCryptPasswordEncoder.encode(password), bio, dateOfBirth.toString().substring(0, 9));
+        ApplicationUser user = new ApplicationUser(firstName, lastName, userName, bCryptPasswordEncoder.encode(password), bio, dateOfBirth.toString().substring(0, 10));
         userRepository.save(user);
         Authentication authentication = new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>());
         SecurityContextHolder.getContext().setAuthentication(authentication);
         return new RedirectView("/");
     }
 
+    /**
+     * 
+     * @param p The Swing security principal.
+     * @param userId The user id.
+     * @param body The post.
+     * @return Redirect to user details route.
+     */
     @PostMapping("/users/{userId}/add-post")
     public RedirectView addPost(
             Principal p,
@@ -106,6 +201,4 @@ public class ApplicationUserController {
         userRepository.save(postingUser);
         return new RedirectView("/users/{userId}");
     }
-
-
 }
